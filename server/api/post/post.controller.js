34c6b700da16c86496ec2068186fa138c891268e
Don <772528797@qq.com>
2017-11-06 18:31:19 +0800

    开发归档查询接口

--- server/api/post/post.controller.js
+++ server/api/post/post.controller.js
@@ -1,6 +1,6 @@
 exports.getPostList = async(ctx) => {
   let page = ctx.query.page || 1,
-    pageNum = ctx.query.pageNum || 10;
+      pageNum = ctx.query.pageNum || 10;
   let pageIndex = (page - 1) * pageNum < 0 ? 0 : (page - 1) * pageNum;
   let sql = ` SELECT post.id, post.title, SUBSTRING_INDEX(post.content, '<!-- more -->', 1) AS content, 
                 post.poster, post.createTime, post.categoryId, category.name AS categoryName 
@@ -24,14 +24,15 @@ exports.getPostList = async(ctx) => {
 }
 
 exports.getPost = async(ctx) => {
-	let id = ctx.params.id || 0,
-      sql = ` SELECT post.id, post.title, REPLACE(post.content, '<!-- more -->', '') AS content, 
+  let id = ctx.params.id || 0,
+    	sql = ` SELECT post.id, post.title, REPLACE(post.content, '<!-- more -->', '') AS content, 
               post.poster, post.createTime, post.categoryId, category.name AS categoryName 
               FROM post LEFT JOIN category ON post.categoryId = category.id 
               WHERE post.status = 'PUBLISHED' AND post.id = ${id}`,
-      tagSql = ` SELECT tagId, tag.name AS tagName from post_tag a LEFT JOIN tag on a.tagId = tag.id 
+    	tagSql = ` SELECT tagId, tag.name AS tagName from post_tag a LEFT JOIN tag on a.tagId = tag.id 
                  WHERE a.postId = ${id}`,
-      prevNextSql = ` SELECT -1 AS sort, id, title FROM post WHERE id > ${id} AND status = 'PUBLISHED' LIMIT 0, 1
+    	prevNextSql = ` SELECT -1 AS sort, id, title FROM (SELECT * FROM post WHERE id > ${id} 
+                        AND status = 'PUBLISHED' LIMIT 0, 1) AS tab
                       UNION
                       SELECT 1 AS sort, id, title FROM (SELECT * FROM post WHERE id < ${id} 
                         AND status = 'PUBLISHED' ORDER BY id DESC LIMIT 0, 1) AS tab`
@@ -41,12 +42,12 @@ exports.getPost = async(ctx) => {
       let tagResults = await ctx.execSql(tagSql);
       let prevNextResults = await ctx.execSql(prevNextSql);
       let prev = {},
-          next = {};
-      for(let value of Object.values(prevNextResults)) {
-        if(value.sort === 1) {
+        next = {};
+      for (let value of Object.values(prevNextResults)) {
+        if (value.sort === 1) {
           Object.assign(next, value);
         }
-        if(value.sort === -1) {
+        if (value.sort === -1) {
           Object.assign(prev, value);
         }
       }
@@ -67,6 +68,32 @@ exports.getPost = async(ctx) => {
         next: null
       };
     }
+  } catch (error) {
+    console.log(error);
+    ctx.body = {
+      success: 0,
+      message: '查询数据出错'
+    };
+  }
+}
+
+exports.getArchive = async(ctx) => {
+  let catSql = `  SELECT category.id, category.name, COUNT(post.id) AS count 
+                    FROM category LEFT JOIN post ON post.categoryId = category.id 
+                    GROUP BY category.id`,
+    	tagSql = `  SELECT tag.id, tag.name, COUNT(post.id) AS count FROM tag 
+                    LEFT JOIN post_tag ON tag.id = post_tag.tagId
+                    LEFT JOIN post ON post_tag.postId = post.id AND post.status = 'PUBLISHED'
+                    GROUP BY tag.id`;
+  try {
+    let catResults = await ctx.execSql(catSql);
+    let tagResults = await ctx.execSql(tagSql);
+    ctx.body = {
+      success: 1,
+      message: '',
+      categories: catResults.length > 0 ? catResults : [],
+      tags: tagResults.length > 0 ? tagResults : []
+    };
   } catch (error) {
     console.log(error);
     ctx.body = {
--- server/api/post/post.js
+++ server/api/post/post.js
@@ -3,5 +3,5 @@ const controller = require('./post.controller');
 
 router.get('/getPostList', controller.getPostList);
 router.get('/getPost/:id', controller.getPost);
-
+router.get('/getArchive', controller.getArchive);
 module.exports = router;
