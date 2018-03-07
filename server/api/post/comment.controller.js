ba86708b121d02fa7a81bb2e37a74716be53c2a5
XNAL <772528797@qq.com>
2018-03-07 23:31:18 +0800

    评论增加楼层

--- server/api/post/comment.controller.js
+++ server/api/post/comment.controller.js
@@ -20,11 +20,15 @@ exports.addComment = async(ctx) => {
     return;
   }
   try {
+    let maxData = await ctx.execSql('SELECT IFNULL(max(number), 0) AS maxNumber FROM comments WHERE postId = ?', commentData.postId);
+    let maxNumber = maxData[0].maxNumber + 1;
+    commentData.number = maxNumber;
     let insert = await ctx.execSql('INSERT INTO comments SET ?', commentData);
     if (insert.affectedRows > 0) {
       ctx.body = {
         success: 1,
-        id: insert.insertId
+        id: insert.insertId,
+        number: maxNumber
       };
     } else {
       ctx.body = {
@@ -43,7 +47,10 @@ exports.addComment = async(ctx) => {
 
 exports.getCommentsByPostId = async(ctx) => {
   let postId = ctx.params.postId || 0,
-    sql = `SELECT comments.*, user.avatar, user.userName, cr.content AS replyContent, cr.createdTime AS replyTime, cru.userName AS replyUserName, cru.avatar AS replyAvatar FROM comments 
+    sql = `SELECT comments.*, user.avatar, user.userName, cr.content AS replyContent, 
+      cr.createdTime AS replyTime, cru.userName AS replyUserName, 
+      cru.avatar AS replyAvatar, cr.number AS replyNumber
+      FROM comments 
       LEFT JOIN user ON comments.userId = user.id
       LEFT JOIN comments cr on comments.replyId = cr.id
       LEFT JOIN user cru on cr.userId = cru.id 
